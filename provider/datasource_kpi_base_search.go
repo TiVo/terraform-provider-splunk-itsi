39a3dee47087996c7b9414d0ee5a604de063f7d2
Maria Lukiianova <mlukiianova@productengine.com>
2024-03-16 00:50:05 +0100

    Kpi base search data source (#407)

--- provider/datasource_kpi_base_search.go
+++ provider/datasource_kpi_base_search.go
@@ -2,55 +2,98 @@ package provider
 
 import (
 	"context"
+	"fmt"
 
-	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-framework/datasource"
+	"github.com/hashicorp/terraform-plugin-framework/datasource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/path"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/hashicorp/terraform-plugin-log/tflog"
 	"github.com/tivo/terraform-provider-splunk-itsi/models"
 )
 
-func DatasourceKPIBaseSearch() *schema.Resource {
-	return &schema.Resource{
-		Description: "Use this data source to get the ID of an available KPI Base Search.",
-		ReadContext: DatasourceKPIBaseSearchRead,
-		Schema: map[string]*schema.Schema{
-			"title": {
-				Type:        schema.TypeString,
+// Ensure interface compliance
+var _ datasource.DataSource = &dataSourceKpiBaseSearch{}
+var _ datasource.DataSourceWithConfigure = &dataSourceKpiBaseSearch{}
+
+type dataSourceKpiBaseSearch struct {
+	client models.ClientConfig
+}
+
+type dataSourceKpiBaseSearchState struct {
+	ID    types.String `tfsdk:"id" json:"_key"`
+	Title types.String `tfsdk:"title" json:"title"`
+}
+
+func NewKpiBaseSearchDataSource() datasource.DataSource {
+	return &dataSourceKpiBaseSearch{}
+}
+
+func (d *dataSourceKpiBaseSearch) Metadata(_ context.Context, req datasource.MetadataRequest, resp *datasource.MetadataResponse) {
+	resp.TypeName = req.ProviderTypeName + "_kpi_base_search"
+}
+
+func (d *dataSourceKpiBaseSearch) Configure(ctx context.Context, req datasource.ConfigureRequest, _ *datasource.ConfigureResponse) {
+	if req.ProviderData == nil {
+		return
+	}
+
+	client, ok := req.ProviderData.(models.ClientConfig)
+	if !ok {
+		tflog.Error(ctx, "Unable to prepare client")
+		return
+	}
+	d.client = client
+}
+
+// KpiSearchDataSource schema
+func (d *dataSourceKpiBaseSearch) Schema(ctx context.Context, req datasource.SchemaRequest, resp *datasource.SchemaResponse) {
+	resp.Schema = schema.Schema{
+		MarkdownDescription: "Use this data source to get the ID of an available KPI Base Search.",
+		Attributes: map[string]schema.Attribute{
+			"id": schema.StringAttribute{
+				MarkdownDescription: "The ID of this resource.",
+				Computed:            true,
+			},
+			"title": schema.StringAttribute{
 				Required:    true,
-				Description: "The title of the KPI Base Search",
+				Description: "The title of the KPI Base Search.",
 			},
 		},
 	}
 }
 
-func DatasourceKPIBaseSearchRead(ctx context.Context, d *schema.ResourceData, m interface{}) (diags diag.Diagnostics) {
-	base := kpiBaseSearchBase(m.(models.ClientConfig), d.Id(), d.Get("title").(string))
+// Read data into the Terraform state
+func (d *dataSourceKpiBaseSearch) Read(ctx context.Context, req datasource.ReadRequest, resp *datasource.ReadResponse) {
+	tflog.Debug(ctx, "Preparing to read KPI BS data source")
+	var config dataSourceKpiBaseSearchState
+
+	resp.Diagnostics.Append(req.Config.Get(ctx, &config)...)
+
+	title := config.Title.ValueString()
+	base := kpiBaseSearchBase(d.client, config.ID.ValueString(), title)
 	b, err := base.Find(ctx)
 	if err != nil {
-		return append(diags, diag.Errorf("%s", err)...)
+		resp.Diagnostics.AddError("Unable to read KPI BS object", err.Error())
+		return
+	}
+	json, err := b.RawJson.ToInterfaceMap()
+	if err != nil {
+		resp.Diagnostics.AddError("Unable to read KPI JSON object", err.Error())
+		return
 	}
 	if b == nil {
-		d.SetId("")
-		return nil
+		resp.Diagnostics.AddAttributeError(
+			path.Root("title"),
+			"KPI BS not found",
+			fmt.Sprintf("KPI BS %q not found", title))
+		return
 	}
 
-	err = populateKPIBaseSearchDatasourceData(b, d)
-	if err != nil {
-		return append(diags, diag.Errorf("%s", err)...)
-	}
-	return
-}
+	state := &dataSourceKpiBaseSearchState{}
 
-func populateKPIBaseSearchDatasourceData(b *models.Base, d *schema.ResourceData) error {
-	interfaceMap, err := b.RawJson.ToInterfaceMap()
-	if err != nil {
-		return err
-	}
+	resp.Diagnostics.Append(marshalBasicTypesByTag("json", json, state)...)
+	resp.Diagnostics.Append(resp.State.Set(ctx, &state)...)
 
-	err = d.Set("title", interfaceMap["title"])
-	if err != nil {
-		return err
-	}
-
-	d.SetId(b.RESTKey)
-	return nil
+	tflog.Debug(ctx, "Finished reading KPI BS data source", map[string]any{"success": true})
 }
