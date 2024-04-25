3b007727c3b6ac08882e1a45631037d30e36ce1d
Dmitry Kruglov <dmkruglov@me.com>
2024-04-24 17:17:15 -0700

    support acceptance tests

--- examples/provider/provider.tf
+++ examples/provider/provider.tf
@@ -10,3 +10,13 @@ provider "itsi" {
   #Disable ssl checks:
   #insecure = true
 }
+
+/*
+  Alternatively, provider configuation can be provided by using the following environment variables:
+    * ITSI_HOST
+    * ITSI_PORT
+    * ITSI_ACCESS_TOKEN
+    * ITSI_USER
+    * ITSI_PASSWORD
+    * ITSI_INSECURE
+*/
