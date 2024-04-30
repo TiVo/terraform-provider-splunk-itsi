d7a47a403e26d9ead6cd4cbbe248ca2d3686cb23
dmitrykruglov <dmkruglov@me.com>
2024-04-29 18:07:47 -0700

    collection data: tests and bugfixes (#442)
    * collection data: tests and bugfixes
    
    * cleanup
    
    * fix a comment

---
+++ provider/testdata/TestAccResourceCollectionDataID/5/collection_data.tf
@@ -0,0 +1,33 @@
+/*
+    TestAccResourceCollectionDataID - Step 5
+    Test that a Duplicate ID error can be caught in case when that ID is managed by another instance of the collection data resource under a different scope.
+*/
+
+resource "itsi_collection_data" "test" {
+  scope = "TestAcc_collection_data_id_test"
+
+  collection {
+    name = itsi_splunk_collection.test.name
+  }
+
+  entry {
+    id   = "banana"
+    data = jsonencode({ name = "banana" })
+  }
+
+}
+
+resource "itsi_collection_data" "test2" {
+  scope = "TestAcc_collection_data_id_test2"
+
+  collection {
+    name = itsi_splunk_collection.test.name
+  }
+
+  entry {
+    id   = "banana"
+    data = jsonencode({ name = "banana", color = "yellow" })
+  }
+
+  depends_on = [itsi_collection_data.test]
+}
