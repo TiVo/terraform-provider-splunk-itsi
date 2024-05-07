939f9fdabda230d3f96f0b926bcdd425a3c3eeda
dmitrykruglov <dmkruglov@me.com>
2024-05-07 13:50:27 -0700

    improve tests and docs (#449)
    * refactoring
    
    * sweep collections
    
    * update the Getting Started guide
    
    * basic acceptance tests for collection data sources
    
    * update dependencies

--- provider/datasource_splunk_search_test.go
+++ provider/datasource_splunk_search_test.go
@@ -2,8 +2,33 @@ package provider
 
 import (
 	"testing"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/tivo/terraform-provider-splunk-itsi/provider/util"
 )
 
 func TestDataSourceSplunkSearchSchema(t *testing.T) {
 	testDataSourceSchema(t, new(dataSourceSplunkSearch))
+}
+
+func TestAccDataSplunkSearch(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		ProtoV6ProviderFactories: providerFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: util.Dedent(`
+					data "itsi_splunk_search" "test" {
+						search {
+							query = "| makeresults count = 1 | eval a=123 | table a"
+							earliest_time = "-1m"
+							timeout       = 120
+						}
+					}
+				`),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr("data.itsi_splunk_search.test", "results", `[{"a":"123"}]`),
+				),
+			},
+		},
+	})
 }
