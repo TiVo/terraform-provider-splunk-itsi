51c0568914bd592169305c884d7cfe99f7fa579b
Dmitry Kruglov <dmkruglov@me.com>
2024-05-07 12:45:34 -0700

    basic acceptance tests for collection data sources

--- provider/datasource_collection_data_test.go
+++ provider/datasource_collection_data_test.go
@@ -2,8 +2,44 @@ package provider
 
 import (
 	"testing"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/tivo/terraform-provider-splunk-itsi/provider/util"
 )
 
 func TestDataSourceCollectionDataSchema(t *testing.T) {
 	testDataSourceSchema(t, new(dataSourceCollectionData))
+}
+
+func TestAccDataCollectionData(t *testing.T) {
+	resource.UnitTest(t, resource.TestCase{
+		ProtoV6ProviderFactories: providerFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: util.Dedent(`
+
+					data "itsi_collection_data" "test" {
+
+						collection {
+							name  = "itsi_team"
+							app   = "SA-ITOA"
+							owner = "nobody"
+						}
+						query = <<-EOT
+							{"object_type": "team", "_key": "default_itsi_security_group"}
+						EOT
+
+						fields = [
+							"_key",
+							"title"
+						]
+
+					}
+				`),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr("data.itsi_collection_data.test", "data", `[{"_key":"default_itsi_security_group","title":"Global"}]`),
+				),
+			},
+		},
+	})
 }
--- provider/datasource_collection_test.go
+++ provider/datasource_collection_test.go
@@ -2,8 +2,32 @@ package provider
 
 import (
 	"testing"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/tivo/terraform-provider-splunk-itsi/provider/util"
 )
 
 func TestDataSourceCollectionSchema(t *testing.T) {
 	testDataSourceSchema(t, new(dataSourceCollection))
+}
+
+func TestAccDataCollection(t *testing.T) {
+	resource.UnitTest(t, resource.TestCase{
+		ProtoV6ProviderFactories: providerFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: util.Dedent(`
+					data "itsi_splunk_collection" "test" {
+						name  = "itsi_team"
+						app   = "SA-ITOA"
+						owner = "nobody"
+					}
+				`),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr("data.itsi_splunk_collection.test", "name", "itsi_team"),
+					resource.TestCheckResourceAttrSet("data.itsi_splunk_collection.test", "fields.#"),
+				),
+			},
+		},
+	})
 }
